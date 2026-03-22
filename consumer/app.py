208779f088a0c20efc459e1bf7dfb87c2b6af0c2
Amruthpn <72870933+Amruthpn@users.noreply.github.com>
2026-03-22 18:49:21 +0530

    Update app.py

--- consumer/app.py
+++ consumer/app.py
@@ -3,17 +3,17 @@ from solace.messaging.resources.topic import TopicSubscription
 
 service = messaging_service.MessagingService.builder() \
     .from_properties({
-        "solace.messaging.transport.host": "your-host",
-        "solace.messaging.service.vpn-name": "default",
+        "solace.messaging.transport.host": "http://localhost:8080",
+        "solace.messaging.service.vpn-name": "kafka-test",
         "solace.messaging.authentication.scheme": "basic",
-        "solace.messaging.authentication.basic.username": "your-username",
-        "solace.messaging.authentication.basic.password": "your-password"
+        "solace.messaging.authentication.basic.username": "kafka-clientuser",
+        "solace.messaging.authentication.basic.password": "kafka-clientuser"
     }).build()
 
 service.connect()
 
 receiver = service.create_direct_message_receiver_builder() \
-    .with_subscriptions(TopicSubscription.of("demo/event")) \
+    .with_subscriptions(TopicSubscription.of("KAFKA/SOLACE")) \
     .build()
 
 receiver.start()
